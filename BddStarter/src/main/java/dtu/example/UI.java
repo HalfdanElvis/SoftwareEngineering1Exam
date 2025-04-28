dc917275c4d2e70941d64e7b53eb982449080278
HalfdanElvis <halfdanelvis@gmail.com>
2025-04-28 16:42:57 +0200

    Update UI.java

--- BddStarter/src/main/java/dtu/example/UI.java
+++ BddStarter/src/main/java/dtu/example/UI.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UI {
 
     static App app = new App();
-    static Scanner input = new Scanner(System.in);
+    static Scanner console = new Scanner(System.in);
 
     public static void main(String args[]){
         app.addEmployee("huba");
@@ -20,14 +20,51 @@ public class UI {
 
                 if (app.getSignedInEmployee() == null){
                     System.out.println("Login:");
-                    username = input.nextLine();
+                    username = console.nextLine();
                 }
                 
                 try {
                     if (app.login(username)) {
-                        System.out.println("We locked in");
                         while (true) {
+                            // Main Menu:
+                            System.out.println("How would you like to proceed?");    
+
+                            System.out.println("1. Create Project");
+                            System.out.println("2. Manage Project");
+                            System.out.println("3. Create Special Activity");
+                            System.out.println("4. Manage Special Activities");
+                            System.out.println("5. Create Employee");
+                            System.out.println("6. Manage Employee");
+                            System.out.println("7. Log hours");
+
+                            System.out.println("Select a number from the list above to proceed.");
                             
+                            String userInput = console.nextLine();
+                            int num = Integer.parseInt(userInput);
+                            
+                            switch (num) {
+                                case 1:
+                                    // Create Project
+                                case 2:
+                                    // Create Project
+                                case 3:
+                                    // Create Project
+                                case 4:
+                                    // Create Project
+                                case 5:
+                                    // Create Project
+                                case 6:
+                                    // Create Project
+                                case 7:
+                                    // Create Project
+                                default:
+                                    break;
+                                
+                            }
+
+
+
+
                         }
                     }
                     break;
@@ -40,7 +77,7 @@ public class UI {
             // New User registration
             System.out.println("User doesn't exist. Create user with username: "+username+" Y/N?");
             while (true) {
-                String userInput = input.nextLine();
+                String userInput = console.nextLine();
                 try {
                     if (app.yesOrNo(userInput)) {
                         app.addEmployee(username);
@@ -53,10 +90,6 @@ public class UI {
                 
             }
         }
-    }
-
-    private static boolean promtUserForRegistration(String input) {
-        return app.yesOrNo(input);
     }
 
 
