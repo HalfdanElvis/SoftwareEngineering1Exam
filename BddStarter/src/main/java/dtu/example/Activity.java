35927ab2966a7ce100457dfb801f37218f8356b3
Mcwad <marcuswadsackett@gmail.com>
2025-04-28 16:46:38 +0200

    activity + workdata begun

--- BddStarter/src/main/java/dtu/example/Activity.java
+++ BddStarter/src/main/java/dtu/example/Activity.java
@@ -1,5 +1,42 @@
 package dtu.example;
 
 public class Activity {
+    private String name;
+
+    private int[] activeWeeks;
+    private int expectedWeeks;
+    private float expectedHours;
+
+    public Activity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getActiveWeeks() {
+        return activeWeeks;
+    }
+
+    public void setActiveWeeks(int[] activeWeeks) {
+        this.activeWeeks = activeWeeks;
+    }
+
+    public int getExpectedWeeks() {
+        return expectedWeeks;
+    }
+
+    public void setExpectedWeeks(int expectedWeeks) {
+        this.expectedWeeks = expectedWeeks;
+    }
+
+    public float getExpectedHours() {
+        return expectedHours;
+    }
+
+    public void setExpectedHours(float expectedHours) {
+        this.expectedHours = expectedHours;
+    }
 
 }
