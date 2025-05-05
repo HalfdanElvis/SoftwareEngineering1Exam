7bca68bf54e1905046ffb908f57e3e378616160b
SimonDamsgaard <Simonpdamsgaard@users.noreply.github.com>
2025-05-05 15:50:06 +0200

    Calendar

---
+++ BddStarter/src/main/java/dtu/example/CalendarHelper.java
@@ -0,0 +1,34 @@
+package dtu.example;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalendarHelper {
+    private Calendar date;
+    Calendar calendar = new GregorianCalendar();
+
+    public CalendarHelper(){
+    }
+    
+    
+
+    public int getWeek(Calendar c){   
+
+        int weekNumber=c.get(Calendar.WEEK_OF_YEAR);
+        return weekNumber;
+    }
+
+
+    public Calendar getDate(int week, int year, int dayOfWeek){
+        
+        if (dayOfWeek == 0){
+            dayOfWeek = 1;
+        }
+        if (year == 0){
+            year = Calendar.getInstance().get(Calendar.YEAR);
+        }
+        Calendar c = new GregorianCalendar();
+        c.setWeekDate(year, week, dayOfWeek);
+        return c;
+    }
+}
---
+++ BddStarter/src/test/java/dtu/example/CalenderSteps.java
@@ -0,0 +1,33 @@
+package dtu.example;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalenderSteps {
+    Calendar date;
+    private CalendarHelper ch = new CalendarHelper();
+    App app = TestHelper.app;
+    Activity act;
+    
+
+    @Given("a date {string} is given")
+    public void dateGiven(String dateString) throws Exception {
+        // Eksempel: "2025-05-05"
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        date = Calendar.getInstance();
+        date.setTime(sdf.parse(dateString));
+    }
+
+    @Then("the week number should be {int}")
+    public void returnWeek(int expectedWeek) {
+        int actualWeek = ch.getWeek(date);
+        assertEquals(expectedWeek, actualWeek);
+    }
+}
