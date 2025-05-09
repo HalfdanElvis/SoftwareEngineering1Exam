6a496dcd4b7874616f8b5b6c8f2ef94c5bcc9dc8
SimonDamsgaard <Simonpdamsgaard@users.noreply.github.com>
2025-05-09 14:58:11 +0200

    Employeeinfo

--- BddStarter/src/main/java/dtu/example/DTO/EmployeeInfo.java
+++ BddStarter/src/main/java/dtu/example/DTO/EmployeeInfo.java
@@ -3,18 +3,19 @@ package dtu.example.DTO;
 import java.util.ArrayList;
 import java.util.List;
 import dtu.example.*;
+import io.cucumber.java.lu.a;
 
-public class EmployeeDTO {
+public class EmployeeInfo {
     private String username;
     private boolean peak = false;
     private List<Activity> activityDTOs = new ArrayList<>();
 
-    public EmployeeDTO(String username, boolean peak, List<Activity> activityDTOs){
+    public EmployeeInfo(String username, boolean peak, List<Activity> activityDTOs){
         this.username = username;
         this.peak = peak; 
         this.activityDTOs = activityDTOs;
     }
-    public EmployeeDTO(Employee employee){
+    public EmployeeInfo(Employee employee){
         this.username = employee.getUsername();
         this.peak = employee.isPeak();
         this.activityDTOs = employee.getActivities();
@@ -23,9 +24,15 @@ public class EmployeeDTO {
     public List<Activity> getActivities(){
         return activityDTOs;
     }
+    public void addActivity(Activity act){
+        activityDTOs.add(act);
+    }
     public boolean isPeak(){
 
         return peak;
+    }
+    public void setPeak(boolean c){
+        this.peak=c;
     }
     
     public Activity getActivity(String activityName){
@@ -36,6 +43,13 @@ public class EmployeeDTO {
             }
         }
         return activity;
+    }
+    public void removeActivity(String activityName){
+        for (int i = 0; i<activityDTOs.size();i++){
+            if (activityDTOs.get(i).getName().equals(activityName)){
+                activityDTOs.remove(i);
+            }
+        }
     }
 
 }
